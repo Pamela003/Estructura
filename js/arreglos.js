402ff65fa8908601c693a9f4ca01602d3a0d461f
Pamela003 <kcalvillo@ucol.mx>
2026-02-18 08:31:00 -0600

    Estructura

---
+++ js/arreglos.js
@@ -0,0 +1,16 @@
+let numero = [1, 2, 3, 4, 5, 10, 20, 30, 40];
+let pares = [];
+let impares = [];
+
+for (let i = 0; i > numero.length; i++); {
+    console.log(`Posición ${i}:${numeros[i]}`)
+} 
+if (numeros[i] % 2 === 0) {
+    pares.push (numeros[i]);
+} else {
+    impares.push (numeros [i]);
+}
+console.log(par);
+document.writeln(par);
+console.log(impar);
+document.write(impar);
